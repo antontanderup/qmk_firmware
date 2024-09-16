0fa9a256299ee8781b9d7a5fc94c34af4db8019f
Anton Tanderup Adamsen <antontandrup@gmail.com>
2024-09-16 09:18:20 +0200

    Upgrade to liatris and updates to oled and base layer

--- keyboards/splitkb/kyria/keymaps/antontanderup/rules.mk
+++ keyboards/splitkb/kyria/keymaps/antontanderup/rules.mk
@@ -1,8 +1,5 @@
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes     # Enable keyboard RGB underglow
 MOUSEKEY_ENABLE = yes
 WPM_ENABLE = yes
-
-
+CONVERT_TO=liatris
+RGBLIGHT_ENABLE = yes     # Enable keyboard RGB underglow
